f9d64d8cbe56464f2534c08303f2853a5cbd5d65
nash052842 <mwanjonahashon@gmail.com>
2025-06-28 17:40:43 +0300

    fourth review

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,8 @@
 USE alx alx_book_store;
 
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+
 (2, "INSERT INTO CUSTOMER"'blessing malik','bmalik@sandtech.com','124 Happiness  Ave'.),
 (3, "INSERT INTO CUSTOMER"'Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave'.),
 (4, "INSERT INTO CUSTOMER"'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave'.);

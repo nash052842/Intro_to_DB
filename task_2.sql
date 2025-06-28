d7c7bb7193713426b47b2d1ecbfde1cff4709812
nash052842 <mwanjonahashon@gmail.com>
2025-06-28 16:15:19 +0300

    seven review

--- task_2.sql
+++ task_2.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE,
-    text  VARCHAR(215)
+    Adress Text VARCHAR(215)
 );
 
 -- ORDERS TABLE

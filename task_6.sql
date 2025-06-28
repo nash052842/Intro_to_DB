a6b3f1ace3c1b0f94ee185014c5ae1f917b69be1
nash052842 <mwanjonahashon@gmail.com>
2025-06-28 17:37:53 +0300

    third review

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,5 @@
 USE alx alx_book_store;
 
-INSERT INTO CUSTOMER(customer_id,customer_name,email,adress)
-VALUES
-(2, 'blessing malik','bmalik@sandtech.com','124 Happiness  Ave'.),
-(3, 'Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave'.),
-(4, 'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave'.);
+(2, "INSERT INTO CUSTOMER"'blessing malik','bmalik@sandtech.com','124 Happiness  Ave'.),
+(3, "INSERT INTO CUSTOMER"'Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave'.),
+(4, "INSERT INTO CUSTOMER"'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave'.);

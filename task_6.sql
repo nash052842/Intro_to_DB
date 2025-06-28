91ad9cec6da3e10813bed73d1f070d2e3413136b
nash052842 <mwanjonahashon@gmail.com>
2025-06-28 17:24:57 +0300

    first review

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+USE alx alx_book_store;
+
+INSERT INTO CUSTOMER(customer_id,customer_name,email,adress)
+VALUES
+(2 blessing malik,bmalik@sandtech.com,124 Happiness  Ave)
+(3 Obed Ehoneah,eobed@sandtech.com,125 Happiness  Ave)
+(4 Nehemial Kamolu,nkamolu@sandtech.com,126 Happiness  Ave)

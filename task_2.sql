090a0bcb85fe2c7d798000c07cafbabd12b9fd18
nash052842 <mwanjonahashon@gmail.com>
2025-06-28 16:17:25 +0300

    eight reviw

--- task_2.sql
+++ task_2.sql
@@ -26,7 +26,6 @@ CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE,
-    Adress Text VARCHAR(215)
 );
 
 -- ORDERS TABLE

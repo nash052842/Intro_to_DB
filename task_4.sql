058db12d9491a5d60ddb6e3acedf363bf33c087c
nash052842 <mwanjonahashon@gmail.com>
2025-06-28 16:47:09 +0300

    fourth review

--- task_4.sql
+++ task_4.sql
@@ -3,10 +3,10 @@ SELECT
     BOOK_AUTHOR, 
     BOOK_orders, 
     BOOK_customers,
-     COLUMN_DEFAULT,
-    EXTRA
+
 FROM 
     INFORMATION_SCHEMA.COLUMNS
+    
 WHERE 
     TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'Books';
